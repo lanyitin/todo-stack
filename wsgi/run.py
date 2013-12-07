ab848e31ddbcce115192221c7a8e84d755529ac3
Lan, Yi-Tin <lanyitin@gmail.com>
2013-12-07 19:42:10 +0800

    on transforming to long polling

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,17 +1,51 @@
-import os, sys, json, sqlite3, base64, time, pymongo, json
-from werkzeug.contrib.profiler import ProfilerMiddleware
-from werkzeug.contrib.profiler import MergeStream
-
-from flask import Flask, request, g, abort, redirect, url_for, render_template
+import os, sys, json, sqlite3, base64, time, pymongo, json , gevent, uuid, json, logging
+from werkzeug.contrib.profiler import ProfilerMiddleware, MergeStream
+from flask import Flask, request, g, abort, redirect, url_for, render_template, make_response
 from flask.ext.assets import Environment, Bundle
-
 from core import TodoStack, Todo
 from Mappers import MapperFactory
+from gevent import queue
+from werkzeug.debug import DebuggedApplication
+from gevent.pywsgi import WSGIServer
 
-app = Flask(__name__)
-assets = Environment(app)
-factory = MapperFactory("mongo")
 
+class StackCommandDispatcher:
+    dispatchers = dict()
+    @classmethod
+    def openDispatcher(clazz, name):
+        if not clazz.dispatchers.has_key(name):
+            clazz.dispatchers[name] = StackCommandDispatcher(name)
+        return clazz.dispatchers[name]
+
+    def __init__(self, stackid):
+        self.logger = logging.getLogger(__name__)
+        self.stackid = stackid;
+        self.cache = dict();
+        self.evt = gevent.event.Event();
+        self.latestCommand = None;
+
+    def new_command(self, sequenceNumber, data):
+        if self.latestCommand is not None:
+            self.cache[self.latestCommand["key"]] = self.latestCommand
+            self.logger.debug("cache" + str(self.cache))
+
+        self.latestCommand = {"key": sequenceNumber, "commands": data}
+        self.logger.debug("update latest command")
+        self.evt.set()
+        self.evt.clear()
+
+    def fetch_command(self, sequenceNumber):
+        commands = list()
+        for key in self.cache:
+            if key > sequenceNumber:
+                self.logger.debug("append" + str(self.cache[key]))
+                commands.append(self.cache[key])
+        if len(commands) > 0:
+            return commands
+        if (self.latestCommand is not None and self.latestCommand["key"] >= sequenceNumber):
+            commands.append(self.latestCommand)
+
+        return commands
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -23,6 +57,9 @@ def get_db():
         db = mongo_db
     return db
 
+app = Flask(__name__)
+assets = Environment(app)
+factory = MapperFactory("mongo")
 
 @app.route('/')
 def createStack():
@@ -33,33 +70,39 @@ def createStack():
 def displayStack(stackName):
     stack = factory.getMapper().findByName(stackName, get_db()) or TodoStack(None, stackName);
     trash_stack = factory.getMapper().findByName(stackName + "_trash", get_db()) or TodoStack(None, stackName + "_trash");
-    return render_template("display_stack.html", stack=stack, trash_stack=trash_stack);
+    response = make_response(render_template("display_stack.html", stack=stack, trash_stack=trash_stack));
+    response.set_cookie("sequenceNumber", str(0));
+    return response;
 
-@app.route('/<stackName>/push', methods=["POST"])
-def pushItem(stackName):
+@app.route('/<stackName>/push/<sequenceNumber>/', methods=["POST"])
+def pushItem(stackName, sequenceNumber):
     stack = factory.getMapper().findByName(stackName, get_db()) or TodoStack(None, stackName);
     stack.push(request.form['item'])
     factory.getMapper().store(stack, get_db())
     todo = stack.peek()
-    return json.dumps({"response": "success", "commands": [{"command": "push", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}}]})
+    command = {"command": "push", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}};
+    StackCommandDispatcher.openDispatcher(stackName).new_command(sequenceNumber, [command])
+    return json.dumps({"response": "success", "commands": [command]})
 
-@app.route('/<stackName>/pop', methods=["GET"])
-def popItem(stackName):
+@app.route('/<stackName>/pop/<sequenceNumber>/', methods=["GET"])
+def popItem(stackName, sequenceNumber):
     stack = factory.getMapper().findByName(stackName, get_db()) or TodoStack(None, stackName);
     trash_stack = factory.getMapper().findByName(stackName + "_trash", get_db()) or TodoStack(None, stackName + "_trash");
     todo = stack.pop()
     trash_stack.push(todo);
     factory.getMapper().store(stack, get_db())
     factory.getMapper().store(trash_stack, get_db())
-    return json.dumps({"response": "success", "commands": [{"command": "pop", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}}]})
+    command = {"command": "pop", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}};
+    StackCommandDispatcher.openDispatcher(stackName).new_command(sequenceNumber, [command])
+    return json.dumps({"response": "success", "commands": [command]})
 
-@app.route('/<stackName>/moveItem/<int:fromIndex>/<int:toIndex>', methods=["GET"])
-def moveItem(stackName, fromIndex, toIndex):
+@app.route('/<stackName>/moveItem/<int:fromIndex>/<int:toIndex>/<sequenceNumber>/', methods=["GET"])
+def moveItem(stackName, fromIndex, toIndex, sequenceNumber):
     stack = factory.getMapper().findByName(stackName, get_db()) or TodoStack(None, stackName);
     stack.moveItem(fromIndex, toIndex);
     factory.getMapper().store(stack, get_db())
     todos = stack.getItems(True)
-    response = ({"response": "success", "commands": []})
+    response = {"response": "success", "commands": []}
     begin = end = 0
     if (fromIndex > toIndex):
         begin = toIndex
@@ -69,19 +112,23 @@ def moveItem(stackName, fromIndex, toIndex):
         begin = fromIndex
     for i in range(begin, end + 1):
         todo = todos[i]
-        response["commands"].append({"command":"update", "data":{"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}})
-        return json.dumps(response)
+        command = {"command":"update", "data":{"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}};
+        response["commands"].append(command)
+    StackCommandDispatcher.openDispatcher(stackName).new_command(sequenceNumber, response["commands"])
+    return json.dumps(response)
 
-@app.route('/<stackName>/removeItem/<int:index>', methods=["GET"])
+@app.route('/<stackName>/removeItem/<int:index>/<sequenceNumber>/', methods=["GET"])
 def removeItem(stackName, index):
     stack = factory.getMapper().findByName(stackName, get_db()) or TodoStack(None, stackName);
     todos = stack.getItems(True)
     todo = todos[index]
     stack.removeItem(index);
     factory.getMapper().store(stack, get_db())
-    return json.dumps({"response": "success", "commands": [{"command": "remove", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}}]})
+    command = {"command": "remove", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}};
+    StackCommandDispatcher.openDispatcher(stackName).new_command(sequenceNumber, [command])
+    return json.dumps({"response": "success", "commands": [command]})
 
-@app.route('/<stackName>/raisePriority/<int:index>', methods=["GET"])
+@app.route('/<stackName>/raisePriority/<int:index>/<sequenceNumber>/', methods=["GET"])
 def raisePriority(stackName, index):
     stack = factory.getMapper().findByName(stackName, get_db()) or TodoStack(None, stackName);
     todos = stack.getItems(True);
@@ -90,7 +137,25 @@ def raisePriority(stackName, index):
     if todo.priority >= 5:
         todo.priority %= 5;
     factory.getMapper().store(stack, get_db())
-    return json.dumps({"response": "success", "commands": [{"command": "update", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}}]})
+    command = {"response": "success", "commands": [{"command": "update", "data": {"id": str(todo.id), "content":todo.content, "priority": todo.priority, "stackid": str(todo.stackid), "order": todo.order}}]};
+    StackCommandDispatcher.openDispatcher(stackName).new_command(sequenceNumber, [command])
+    return json.dumps({"response": "success", "commands": [command]})
+
+@app.route('/<stackName>/fetch/<sequenceNumber>/', methods=["GET"])
+def fetch(stackName, sequenceNumber):
+    request_sequence_number = sequenceNumber 
+    commands = StackCommandDispatcher.openDispatcher(stackName).fetch_command(request_sequence_number)
+    sequenceNumbers = list()
+    for x in commands:
+        logging.debug(x)
+        if x is not None:
+            sequenceNumbers.append(x["key"])
+    sequenceNumber = 0
+    if len(sequenceNumbers) > 0:
+        sequenceNumber = max(sequenceNumbers)
+
+    response = make_response(json.dumps({"request_sequence_number": request_sequence_number, "command_update_sequence_number": sequenceNumber, "commands": commands}))
+    return response
 
 
 @app.teardown_appcontext
@@ -104,4 +169,7 @@ if __name__ == "__main__":
     stream = MergeStream(sys.stdout, f)
     app.config['PROFILE'] = True
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, stream)
-    app.run(debug = "True")
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    app.debug = True
+    http = WSGIServer(('', 5000),  DebuggedApplication(app));
+    http.serve_forever();
--- wsgi/static/js/stacktodos.js
+++ wsgi/static/js/stacktodos.js
@@ -1,3 +1,11 @@
+var sequenceNumber;
+
+function getSequenceNumber() {
+    return sequenceNumber;
+}
+function setSequenceNumber(n) {
+    sequenceNumber = n;
+}
 var todoTemplate = "" + 
 "<div class=\"list-group-item todo container\" data-todo-priority=\"<%= todo.priority %>\" data-todo-order=\"<%= todo.order %>\" data-todo-id=\"<%= todo.id %>\">" +
 "    <div class=\"glyphicon glyphicon-sort sort icon pull-left\"></div>" +
@@ -37,39 +45,70 @@ function hideDeleteButtons() {
         }
     });
 }
-function handleResponse(response) {
-    response = JSON.parse(response);
-    console.log(response);
-    if (response.response === "success") {
-        for(var i = 0; i < response.commands.length; i++) {
-            var command = response.commands[i];
-            if (command.command === "push") {
-                var stack = $(".stack:not(.trash)");
-                var domStr = compiledTodoTemplate({todo: command.data});
-                stack.prepend(domStr);
-                $("#control-todo-content").val("");
-                if ($(".delete.btn:not(.control):last").css("display") === "none") {
-                    hideDeleteButtons();
-                }
-                if ($(".sort.icon:last").css("display") === "none") {
-                    hideSortIcons();
-                }
-            } else if (command.command === "update") {
-                var todoDom = $(".stack .todo[data-todo-id=" + command.data.id + "]");
-                console.log(todoDom);
-                todoDom.attr("data-todo-order", command.data.order);
-                todoDom.attr("data-todo-priority", command.data.priority);
-            } else if (command.command === "delete") {
-                $(".stack .todo[data-todo-id=" + command.data.id + "]").remove();
-            } else if (command.command === "pop") {
-                var todo = $(".stack:not(.trash) .todo:first");
-                todo.select(".delete").remove();
-                todo.select(".sort").remove();
-                todo.select(".priority").remove();
-                $(".trash.stack").append(todo);
-                if ($(".trash.stack .todo:first").css("display") === "none") {
-                    hideItemsInTrashStackExceptLastNItems(2);
-                }
+
+function setCookie(c_name,value,exdays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name)
+{
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1)
+    {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1)
+    {
+        c_value = null;
+    }
+    else
+    {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1)
+        {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+}
+
+function handleCommands(commandsTimePair) {
+    for(var i = 0; i < commandsTimePair.length; i++) {
+        var command = commandsTimePair[i];
+        if (command.command === "push") {
+            var stack = $(".stack:not(.trash)");
+            var domStr = compiledTodoTemplate({todo: command.data});
+            stack.prepend(domStr);
+            $("#control-todo-content").val("");
+            if ($(".delete.btn:not(.control):last").css("display") === "none") {
+                hideDeleteButtons();
+            }
+            if ($(".sort.icon:last").css("display") === "none") {
+                hideSortIcons();
+            }
+            bindUIEventHandlerToTodoView();
+
+        } else if (command.command === "update") {
+            var todoDom = $(".stack .todo[data-todo-id=" + command.data.id + "]");
+            todoDom.attr("data-todo-order", command.data.order);
+            todoDom.attr("data-todo-priority", command.data.priority);
+        } else if (command.command === "delete") {
+            $(".stack .todo[data-todo-id=" + command.data.id + "]").remove();
+        } else if (command.command === "pop") {
+            var todo = $(".stack:not(.trash) .todo:first");
+            todo.select(".delete").remove();
+            todo.select(".sort").remove();
+            todo.select(".priority").remove();
+            $(".trash.stack").append(todo);
+            if ($(".trash.stack .todo:first").css("display") === "none") {
+                hideItemsInTrashStackExceptLastNItems(2);
             }
         }
     }
@@ -99,18 +138,12 @@ function showItemsInTrashStackExceptLastNItems(num) {
 
 function initControls() {
     $(".control.pop").click(function() {
-        $.ajax({
-            url: "/" + window.stackName + "/pop"
-        }).done(function (response) {
-            handleResponse(response);
-        });
+        $.ajax({ url: "/" + window.stackName + "/pop/" + getSequenceNumber() + "/" });
     });
     $(".control.push").click(function() {
         $.ajax({
-            url: "/" + window.stackName + "/push", type:"POST",
+            url: "/" + window.stackName + "/push/" + getSequenceNumber() + "/" , type:"POST",
             data:{"item":$("#control-todo-content").val()}
-        }).done(function (response) {
-            handleResponse(response);
         });
     });
     $(".control.delete").click(function() {
@@ -142,11 +175,7 @@ function initControls() {
         update: function ( event, ui) {
             var from = ui.item.data("start_pos");
             var to = ui.item.index();
-            $.ajax({
-                url: "/" + window.stackName + "/moveItem/" + from + "/" + to
-            }).done(function (response) {
-                handleResponse(response);
-            });
+            $.ajax({ url: "/" + window.stackName + "/moveItem/" + from + "/" + to + "/" + getSequenceNumber() });
         },
         revert: true,
         handle: ".sort.icon"
@@ -158,24 +187,46 @@ function bindUIEventHandlerToTodoView() {
         var currentPriority = $(e.target).attr("data-todo-priority");
         var todoId = $(e.target).attr("data-todo-id");
         var index = $(e.target).parent().index();
-        $.ajax({url: "/" + window.stackName + "/raisePriority/" + index}).done(function (){
+        $.ajax({url: "/" + window.stackName + "/raisePriority/" + index + "/" + getSequenceNumber() + "/"}).done(function (){
             $(e.target).html((parseInt($(e.target).html()) + 1) % 5);
         });
     });
 
     $(".stack:not(.trash) .todo .delete").unbind().click(function (e){
-        $.ajax({url: "/" + window.stackName + "/removeItem/" + $(e.target).parent().index()}).done(function () {
+        $.ajax({url: "/" + window.stackName + "/removeItem/" + $(e.target).parent().index() + "/" + getSequenceNumber() + "/"}).done(function () {
             $(e.target).parent().remove();
         });
 
     });
 }
 (function() {
-
+    setSequenceNumber(getCookie("sequenceNumber") || 0);
     initControls();
     bindUIEventHandlerToTodoView();
     hideItemsInTrashStackExceptLastNItems(2);
     hideSortIcons();
     hideDeleteButtons();
+
+    var handleFetchResponse = function (data) {
+        data = JSON.parse(data);
+        setSequenceNumber(parseInt(data.command_update_sequence_number) + 1);
+        for (var i = 0; i < data.commands.length; i++) {
+            if (data.commands[i] !== null) {
+                var commands = data.commands[i].commands;
+                if (commands !== undefined) {
+                    handleCommands(commands);
+                }
+            }
+        }
+    }
+
+    console.log($.ajax({url: "/" + window.stackName + "/fetch/" + getSequenceNumber() + "/"}).done(function (data) {
+        handleFetchResponse(data);
+    }));
+    setInterval(function () {
+        console.log($.ajax({url: "/" + window.stackName + "/fetch/" + getSequenceNumber() + "/"}).done(function (data) {
+            handleFetchResponse(data);
+        }));
+    }, 1000 * 10);
 
 })();
